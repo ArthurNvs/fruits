bf09dfc32db7cb947c60db03405f67ad5664cabe
Arthur Neves <arthur.monteiro.ext@sodexo.com>
2021-11-06 12:15:04 -0300

    creating repository

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Arthur Neves on 06/11/21.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
