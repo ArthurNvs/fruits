5770f9b1f9a167d63b10b513b88102677b095299
Arthur Neves <arthur.monteiro.ext@sodexo.com>
2021-11-06 18:53:05 -0300

    New OnboardingView with TabView and FruitCards template

---
+++ Fruits/App/OnboardingView.swift
@@ -0,0 +1,26 @@
+//
+//  OnboardingView.swift
+//  Fruits
+//
+//  Created by Arthur Neves on 06/11/21.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+      TabView {
+        ForEach(0..<5) { item in
+          FruitCardView()
+        }
+      } // TabView
+      .tabViewStyle(PageTabViewStyle())
+      .padding(.vertical, 20)
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
