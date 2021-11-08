a3b39910959b8ae1b43313059c2977fbc15dd4ba
Arthur Neves <arthur.monteiro.ext@sodexo.com>
2021-11-08 14:51:02 -0300

    Fruits detail view implemented

---
+++ Fruits/App/FruitDetailView.swift
@@ -0,0 +1,57 @@
+//
+//  FruitDetailView.swift
+//  Fruits
+//
+//  Created by Arthur Neves on 07/11/21.
+//
+
+import SwiftUI
+
+struct FruitDetailView: View {
+  var fruit: Fruit
+  
+  var body: some View {
+    NavigationView {
+      ScrollView(.vertical, showsIndicators: false) {
+        VStack(alignment: .center, spacing: 20) {
+          FruitHeaderView(fruit: fruit)
+          VStack(alignment: .leading, spacing: 20) {
+
+            Text(fruit.title)
+              .font(.largeTitle)
+              .fontWeight(.heavy)
+              .foregroundColor(fruit.gradientColors[1])
+
+            Text(fruit.headline)
+              .font(.headline)
+              .multilineTextAlignment(.leading)
+            
+            // MARK: - NUTRIENTS
+
+            Text("Learn more about \(fruit.title)".uppercased())
+              .fontWeight(.bold)
+              .foregroundColor(fruit.gradientColors[1])
+
+            Text(fruit.description)
+              .multilineTextAlignment(.leading)
+            
+            SourceLinkView()
+              .padding(.top, 10)
+              .padding(.bottom, 40)
+          }// VStack
+          .padding(.horizontal, 20)
+          .frame(maxWidth: 640, alignment: .center)
+        } // VStack
+        .navigationBarTitle(fruit.title, displayMode: .inline)
+        .navigationBarHidden(true)
+      } // ScrollView
+      .edgesIgnoringSafeArea(.top)
+    } // NavigationView
+  }
+}
+
+struct FruitDetailView_Previews: PreviewProvider {
+  static var previews: some View {
+    FruitDetailView(fruit: fruitsData[0])
+  }
+}
